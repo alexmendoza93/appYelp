c6df6e4ae0e9988f115fec4082d28f8d69517fe6
alex Mendoza <tiburon9386@gmail.com>
2021-08-26 18:26:19 -0500

    acabamos de renderizar nuestro template home desde nuestro servidor

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
+// con estos dos hechamos a andar nuestro servidor
 // const mongoose = require('mongoose');
+const path = require('path');
+// con esto nos evitamos problemas de ejecucion
+
+app.set('view engine', 'ejs');
+// con esto hacemos funcionar nuestros templates de ejs
+app.set('views', path.join(__dirname, 'views'));
+// avisamos que estamos utilizando la carpeta views
 
 app.get('/', (req, res) => {
-    res.send('Hola desde campamento yelp')
-})
+    // res.send('Hola desde campamento yelp')
+    // este mensaje es de calis para probar que estemos conectados con la pagina
+    res.render('home')
+});
 
 app.listen(3000, () => {
     console.log('en linea puerto 3000')
-})
+});
