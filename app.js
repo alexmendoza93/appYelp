d979dd509011945b8211ca7b47b56709326221ab
alex Mendoza <tiburon9386@gmail.com>
2021-08-27 18:08:39 -0500

    Ahora ya podemos editar nuestros campos, instalamos method-override y lo implementamos exitosamente

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 // de esta forma mandamos a llamar a nuestro modelo y podemos empeza a hacer objetos
 
@@ -30,6 +31,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended: true}));
 // con esto parseamos por si queremos visualizar un post en una pagina
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     // res.send('Hola desde campamento yelp')
@@ -64,6 +66,23 @@ app.post('/campgrounds', async (req, res) => {
     // con esto podemos ver el post si esta parseado el pedo
 });
 
+// ---------------------------------------------
+// ahora queremos editar los campamentos
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground });
+});
+// -----------------------------------------------
+// vamos a calar si sirve el metodo override y con esto vamos a actualizar realmente la base de datos
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    // obtenemos el id
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`)
+    // res.send('Eres Un Perro Dios!!!')
+    // mandamos ese mansaje si funciona
+});
+
 
 // --------------------------------------------
 // iniciamos con nuestra pagina a detalle con id
@@ -73,7 +92,7 @@ app.get('/campgrounds/:id', async (req, res) => {
     res.render('campgrounds/show', {campground})
     // y de esta forma pasamos el id a Show.ejs
 });
-// ---------------------------------------------
+// --------------------------------------------
 // hacemos nuestro primer campamento
 // app.get('/makecampground', async (req, res) => {
 //     const camp = new campground({ title: 'Mi Patio Trasero', description: 'un sitio muy comodo'});
